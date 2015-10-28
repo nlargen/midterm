8952d5968f88e63b7b39ebaaabd1015fa0c92bf2
Nicholas Largen <nicholaslargen@nicholass-macbook-pro-3.local>
2015-10-28 13:50:28 -0500

    Initial Commit

---
+++ CSC390Midterm WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CSC390Midterm WatchKit Extension
+//
+//  Created by Nicholas Largen on 10/28/15.
+//  Copyright © 2015 Nicholas Largen. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ CSC390Midterm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSC390Midterm
+//
+//  Created by Nicholas Largen on 10/28/15.
+//  Copyright © 2015 Nicholas Largen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
